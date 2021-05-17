3397c910ab17adb2456393dc42bf5eeaa2f9c8b5
Kamal Rathi <kamalrathi049@gmail.com>
2021-05-17 17:27:41 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Container, Spinner, Button } from 'react-bootstrap'
 
-function App() {
+import './App.css'
+import DarkMode from "./components/dark-mode/DarkMode.component.jsx";
+import useFetchJobs from './components/useFetchJobs'
+import Job from './components/Job'
+import JobsPagination from './components/JobsPagination'
+import SearchForm from './components/SearchForm'
+
+const App = () => {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  const handleParamChange = (e) => {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      
+      <Container className='my-4'>
+        <div className="header">
+        <h1 className='mb-4'>Developer Jobs</h1>
+        <DarkMode/>
+        </div>
+        <SearchForm params={params} onParamChange={handleParamChange} />
+        <JobsPagination
+          page={page}
+          setPage={setPage}
+          hasNextPage={hasNextPage}
+        />
+        {loading && (
+          <Button
+            variant='outline-success'
+            disabled
+            size='lg'
+            block
+            className='mb-3'
+          >
+            <Spinner
+              as='span'
+              animation='grow'
+              variant='success'
+              size='sm'
+              role='status'
+              aria-hidden='true'
+            />
+            Gathering Avaliable Jobs
+          </Button>
+        )}
+        {error && (
+          <Button variant='outline-warning' size='lg' block className='mb-3'>
+            <p>sorry, kindly refresh or check your internet connection</p>
+          </Button>
+        )}
+        {jobs
+        .filter((job,idx) => idx < 20)
+        .map((job) => {
+          return <Job key={job.id} job={job} />
+        })}
+        <JobsPagination
+          page={page}
+          setPage={setPage}
+          hasNextPage={hasNextPage}
+        />
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
